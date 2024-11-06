52a6275896e7aabcdce41b2e4feefb7eefd65c48
TeixeiraManuel <teixeiramanuelteidy@gmail.com>
2024-11-06 10:33:31 -0300

    feat:add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
-export default function LoginPage() {
+const LoginPage = async () => {
+  const { userId } = await auth();
+  if (userId) {
+    redirect("./");
+  }
   return (
     <div className="grid h-full grid-cols-2">
       {/*ESQUERDA*/}
@@ -15,15 +22,17 @@ export default function LoginPage() {
           className="mb-8"
         />
         <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="text-muted-foreground">
+        <p className="mb-8 text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant="outline">
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
       {/*DIREITA*/}
       <div className="relative h-full w-full">
@@ -36,4 +45,5 @@ export default function LoginPage() {
       </div>
     </div>
   );
-}
+};
+export default LoginPage;
