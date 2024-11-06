35b799282b46dbfd304c293a034f0159da12d057
TeixeiraManuel <teixeiramanuelteidy@gmail.com>
2024-11-06 06:18:37 -0300

    chore:add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
-  return (
-    <div>
-      <h1 className="text-green-600">hello</h1>
-    </div>
-  );
+  return <Button>Hello world</Button>;
 }
